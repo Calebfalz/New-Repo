d71ab5ef8c34bff26c4354d482987a271d852f0a
Calebfalz <faleyecassiel005@gmail.com>
2021-04-25 23:52:47 +0100

    Initial state of the app

---
+++ worldtime_app/lib/pages/locations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Locations extends StatefulWidget {
+  Locations({Key key}) : super(key: key);
+
+  @override
+  _LocationsState createState() => _LocationsState();
+}
+
+class _LocationsState extends State<Locations> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Locations'));
+  }
+}
