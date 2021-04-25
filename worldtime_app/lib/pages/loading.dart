c539ff595e1c1ccc6056c8f67e52dae65a7cbd92
Calebfalz <faleyecassiel005@gmail.com>
2021-04-26 00:49:50 +0100

    worldtime api

--- worldtime_app/lib/pages/loading.dart
+++ worldtime_app/lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   Loading({Key key}) : super(key: key);
@@ -8,16 +9,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    var url = Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Lagos');
+    http.Response response = await http.get(url);
+    print(response.body);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getData();
     print('This is my state');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('BUild state');
     return Scaffold(
         body: SafeArea(
       child: Column(
