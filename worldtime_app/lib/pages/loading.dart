ac421af2ab5383a227043099d0cf8c731b37be67
Calebfalz <faleyecassiel005@gmail.com>
2021-04-26 01:18:12 +0100

    New commit

--- worldtime_app/lib/pages/loading.dart
+++ worldtime_app/lib/pages/loading.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class Loading extends StatefulWidget {
   Loading({Key key}) : super(key: key);
@@ -8,16 +12,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    var url = Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Lagos');
+    http.Response response = await http.get(url);
+    dynamic data = jsonDecode(response.body);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+    String newOffset = offset.substring(1, 3);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(newOffset)));
+    print(now);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getData();
     print('This is my state');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('BUild state');
     return Scaffold(
         body: SafeArea(
       child: Column(
--- worldtime_app/lib/services/worldtime_api.dart
+++ worldtime_app/lib/services/worldtime_api.dart
@@ -1,1 +1,27 @@
-class WorldtimeApi {}
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WorldtimeApi {
+  String url;
+  String flag;
+  String location;
+  String time;
+
+  WorldtimeApi({this.url, this.flag, this.location});
+
+  Future<void> mainData() async {
+    try {
+      var url = Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Lagos');
+      http.Response response = await http.get(url);
+      dynamic data = jsonDecode(response.body);
+
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'];
+      String newOffset = offset.substring(1, 3);
+
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(newOffset)));
+      print(now);
+    } catch (e) {}
+  }
+}
